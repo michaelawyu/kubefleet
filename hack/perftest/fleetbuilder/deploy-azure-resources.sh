f723da6fe6b8447eb000eb4fa403ba86f8815039
michaelawyu <chenyu1@microsoft.com>
2026-04-17 09:33:43 +1000

    Minor fixes
    Signed-off-by: michaelawyu <chenyu1@microsoft.com>

--- hack/perftest/fleetbuilder/deploy-azure-resources.sh
+++ hack/perftest/fleetbuilder/deploy-azure-resources.sh
@@ -5,6 +5,8 @@ RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME:?Environment variable RESOURCE_GROUP_N
 LOCATION=${LOCATION:?Environment variable LOCATION is not set}
 REGISTRY_NAME_WO_SUFFIX=${REGISTRY_NAME_WO_SUFFIX:?Environment variable REGISTRY_NAME_WO_SUFFIX is not set}
 VNET_NAME=${VNET_NAME:?Environment variable VNET_NAME is not set}
+STORAGE_ACCOUNT_NAME=${STORAGE_ACCOUNT_NAME:?Environment variable STORAGE_ACCOUNT_NAME is not set}
+QUEUE_NAME=${QUEUE_NAME:?Environment variable QUEUE_NAME is not set}
 CUSTOM_TAGS=${CUSTOM_TAGS:-perf_test=true}
 
 # Create an Azure resource group.
@@ -33,3 +35,18 @@ az network vnet create \
     --subnet-name "default" \
     --subnet-prefixes "10.0.0.0/16" \
     --tags "$CUSTOM_TAGS"
+
+# Create an Azure storage account.
+echo "Creating storage account $STORAGE_ACCOUNT_NAME in resource group $RESOURCE_GROUP_NAME..."
+az storage account create \
+    -n "$STORAGE_ACCOUNT_NAME" \
+    -g "$RESOURCE_GROUP_NAME" \
+    -l "$LOCATION" \
+    --sku Standard_LRS \
+    --tags "$CUSTOM_TAGS"
+
+# Create an Azure storage queue.
+echo "Creating storage queue $QUEUE_NAME in storage account $STORAGE_ACCOUNT_NAME..."
+az storage queue create \
+    -n "$QUEUE_NAME" \
+    --account-name "$STORAGE_ACCOUNT_NAME"
